14122273b07e2cf804c0f4ad545719f5f94dcde6
kiang <kiange@gmail.com>
2022-02-28 21:25:01 +0800

    regenerate csv files from file based to folder based

--- 02_kml2csv.php
+++ 02_kml2csv.php
@@ -2,14 +2,14 @@
 foreach (glob(__DIR__ . '/kml/*.kml') as $kmlFile) {
     $p = pathinfo($kmlFile);
     $xml = simplexml_load_file($kmlFile);
-    $csvFile = __DIR__ . '/csv/' . $p['filename'] . '.csv';
-    $fh = fopen($csvFile, 'w');
-    $headerDone = false;
-    $header = [];
     if (isset($xml->Document->Folder)) {
         foreach ($xml->Document->Folder as $folder) {
+            $csvFile = __DIR__ . '/csv/' . $p['filename'] . '_' . (string)$folder->name . '.csv';
+            $fh = fopen($csvFile, 'w');
+            $headerDone = false;
+            $header = [];
             foreach ($folder->Placemark as $placemark) {
-                if(!isset($placemark->ExtendedData)) {
+                if (!isset($placemark->ExtendedData)) {
                     continue;
                 }
                 $line = [];
