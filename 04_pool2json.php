9f7ba2ad40628eb6f9c887f14e9d4475cae0336f
kiang <kiange@gmail.com>
2022-03-02 21:35:11 +0800

    generate geojson

---
+++ 04_pool2json.php
@@ -0,0 +1,29 @@
+<?php
+$jsonPath = __DIR__ . '/docs/json';
+if (!file_exists($jsonPath)) {
+    mkdir($jsonPath, 0777, true);
+}
+foreach (glob(__DIR__ . '/csv/pool/*.csv') as $csvFile) {
+    $fh = fopen($csvFile, 'r');
+    $head = fgetcsv($fh, 2048);
+    $fc = [
+        'type' => 'FeatureCollection',
+        'features' => [],
+    ];
+    while ($line = fgetcsv($fh, 2048)) {
+        $f = [
+            'type' => 'Feature',
+            'properties' => json_decode($line[2], true),
+            'geometry' => [
+                'type' => 'Point',
+                'coordinates' => [
+                    $line[1], $line[0]
+                ],
+            ],
+        ];
+        $fc['features'][] = $f;
+    }
+    $p = pathinfo($csvFile);
+    $jsonFile = $jsonPath . '/' . $p['filename'] . '.json';
+    file_put_contents($jsonFile, json_encode($fc, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE));
+}
