14b988237f6f60402ebf1314ec0ca32992d5929e
kiang <kiange@gmail.com>
2022-03-07 00:08:35 +0800

    folder name may be the same, add surfix to prevent overwriting

--- 02_kml2csv.php
+++ 02_kml2csv.php
@@ -1,10 +1,22 @@
 <?php
+$pairs = [
+    '.' => '_'
+];
 foreach (glob(__DIR__ . '/kml/*.kml') as $kmlFile) {
     $p = pathinfo($kmlFile);
     $xml = simplexml_load_file($kmlFile);
     if (isset($xml->Document->Folder)) {
+        $folderNameCount = [];
         foreach ($xml->Document->Folder as $folder) {
-            $csvFile = __DIR__ . '/csv/' . $p['filename'] . '_' . (string)$folder->name . '.csv';
+            $folderName = (string)$folder->name;
+            $folderName = strtr($folderName, $pairs);
+            if(!isset($folderNameCount[$folderName])) {
+                $folderNameCount[$folderName] = 0;
+                $csvFile = __DIR__ . '/csv/' . $p['filename'] . '_' . $folderName . '.csv';
+            } else {
+                ++$folderNameCount[$folderName];
+                $csvFile = __DIR__ . '/csv/' . $p['filename'] . '_' . $folderName . $folderNameCount[$folderName] . '.csv';
+            }
             $fh = fopen($csvFile, 'w');
             $headerDone = false;
             $header = [];
